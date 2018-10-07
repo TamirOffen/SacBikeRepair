2b543231d83375ba6883da6b9c2b454ccaf39ab8
Tamir Offen <tamiroffen@gmail.com>
2018-10-07 13:53:20 -0700

    Finished php contact form

--- contact_form_process.php
+++ contact_form_process.php
@@ -1,14 +1,33 @@
 <?php
 
+session_start(); 
+
+date_default_timezone_set('America/Los_Angeles');
+$date = date('m/d/Y h:i a', time());
+
 if(isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $subject = $_POST['subject'];
-  $mailFrom = $_POST['mail'];
+
+  if(empty($_POST['first_name'])) {
+    die("You need to enter your first name");
+  }
+  if(empty($_POST['last_name'])) {
+    die("You need to enter your last name");
+  }
+  if(empty($_POST['email'])) {
+   die("You need to enter your email");
+  }
+  if(empty($_POST['message'])) {
+    die("You need to enter your message");
+  }
+
+  $name = '"' . $_POST['first_name'] . ' ' . $_POST['last_name'] . '"';
+  $subject = 'Contact ' . $date;
+  $mailFrom = $_POST['email'];
   $message = $_POST['message'];
 
-  $mailTo = "tamiroffen@gmail.com";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have recieved an email from ".$name.".\n\n".$message;
+  $mailTo = "tamiroffen@sacbikerepair.com";
+  $headers = "From: " . $mailFrom;
+  $txt = "You have recieved an email from " . $name . ".\n\n" .  $message;
 
   mail($mailTo, $subject, $txt, $headers);
 
